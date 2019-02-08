10b7cac7d3383da32f34abca37ea415ed85f5d7b
binaryDiv <binarydiv@gmail.com>
2019-02-08 23:59:36 +0100

    implement public status HTTP server on port 8080

--- opt/afra_door/lock_server.js
+++ opt/afra_door/lock_server.js
@@ -10,12 +10,24 @@ var key_ble = new keyble.Key_Ble({
 	user_id: 1, // The user ID
 	user_key: fs.readFileSync("/opt/afra_door/secrets/user.key", "utf8"),
 	auto_disconnect_time: 0, // After how many seconds of inactivity to auto-disconnect from the device (0 to disable)
-	status_update_time: 120 // Automatically check for status after this many seconds without status updates (0 to disable)
+	status_update_time: 60 // Automatically check for status after this many seconds without status updates (0 to disable)
 });
 
 var shared_secret = fs.readFileSync("/opt/afra_door/secrets/shared_secret", "utf8")
 
 console.log("Connected to AFRA lock")
+
+var current_door_status = "UNKNOWN";
+
+// Listen for door status changes
+key_ble.on("status_change", (new_status_id, new_status_string) => {
+	console.log("door status changed:", new_status_string);
+	current_door_status = new_status_string;
+});
+
+function status_door() {
+	return current_door_status;
+}
 
 function lock_door(){
 	ts = Math.round((new Date()).getTime() / 1000)
@@ -39,6 +51,7 @@ function unlock_door(){
 	});
 }
 
+// HTTP server on port 8001, only listening on localhost
 http.createServer(function (req, res) {
 	var q = new url.URL("http://127.0.0.1" + req.url)
 
@@ -56,4 +69,11 @@ http.createServer(function (req, res) {
 		console.log("Request rejected")
 	}
 	res.end(); //end the response
-}).listen(8001, "127.0.0.1"); //the server object listens on port 8080
+}).listen(8001, "127.0.0.1");
+
+// Public HTTP server on port 8080, only for door status
+http.createServer(function (req, res) {
+	//console.log("Request on public port 8080");
+	res.write("Door status: " + status_door() + "\n");
+	res.end();
+}).listen(8080);
