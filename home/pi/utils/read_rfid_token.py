e72777d41f5fdd473252fded8003b39979617452
binaryDiv <binarydiv@gmail.com>
2019-02-26 19:58:45 +0100

    read_rfid_token.py: catch KeyboardInterrupt

--- home/pi/utils/read_rfid_token.py
+++ home/pi/utils/read_rfid_token.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-import evdev, requests, hashlib
+import evdev, requests, hashlib, sys
 
-rfid_reader = evdev.InputDevice('/dev/input/event0')
-
-print("Connected to RFID Reader")
-
-current_code = ""
 keys = "XX1234567890XXXXqwertzuiopXXXXasdfghjklXXXXXyxcvbnmXXXXXXXXXXXXXXXXXXXXXXX"
 
-for event in rfid_reader.read_loop():
-	if event.type == 1 and event.value == 1:
-		if event.code > len(keys):
-			continue
+try:
+	rfid_reader = evdev.InputDevice('/dev/input/event0')
+	print("Connected to RFID Reader")
 
-		if keys[event.code] in "0123456789":
-			current_code += keys[event.code]
-		else:
-			md5_code = hashlib.md5(current_code.encode()).hexdigest()
-			print(current_code + " " + md5_code)
-			current_code = ""
+	current_code = ""
+	for event in rfid_reader.read_loop():
+		if event.type == 1 and event.value == 1:
+			if event.code > len(keys):
+				continue
+
+			if keys[event.code] in "0123456789":
+				current_code += keys[event.code]
+			else:
+				md5_code = hashlib.md5(current_code.encode()).hexdigest()
+				print(current_code + " " + md5_code)
+				current_code = ""
+except KeyboardInterrupt:
+	sys.exit(0)
